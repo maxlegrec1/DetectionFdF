b77f5e28bf2d7a54a1b352113921e0100a438450
Leo Sonnet <leo.sonnet@student-cs.fr>
2022-11-18 23:09:00 +0100

    réseau fonctionnel sur le dataset

--- dataset_for_nn.py
+++ dataset_for_nn.py
@@ -7,17 +7,25 @@ import numpy as np
  
 # get the path/directory
 def create_data_list(folder):
+    i = 0
     folder_dir = "Dataset_spec/"+folder
     data_list, label_list = [],[]
     for images in os.listdir(folder_dir):
-        data_list.append(Image.open("Dataset_spec/"+folder+"/"+images))
+        data_list.append(np.asarray(Image.open("Dataset_spec/"+folder+"/"+images)))
         # check if the image ends with png
         if (images.startswith("Autre")):
             label_list.append(0)
         else:
             label_list.append(1)
+        i+=1
+        if i % 500 == 0:
+            print(i)
 
-    return (data_list, label_list)
+    label_array = np.array(label_list)
+    print("label ok")
+    data_array = np.array( data_list )
 
+    print("data ok")
 
+    return data_array, label_array
         
--- reseau_neurone.py
+++ reseau_neurone.py
@@ -5,17 +5,23 @@ import tensorflow as tf
 from conf_matrix import show_confusion_matrix
 from sklearn.metrics import confusion_matrix
 from dataset_for_nn import create_data_list
+from shuffle import unison_shuffled_copies
 
-(train_X, train_y), (val_X, val_Y), (test_X, test_y) = create_data_list('Training'), create_data_list('Validation'), create_data_list("Test")
+(train_X, train_y)= unison_shuffled_copies(create_data_list('Training'))
+(val_X, val_Y) = unison_shuffled_copies(create_data_list('Validation'))
+(test_X, test_y) = unison_shuffled_copies(create_data_list("Test"))
+
 
 
 model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Conv2D(16, 3, activation = 'relu', input_shape = (645,1168,3)))
+model.add(tf.keras.layers.MaxPooling2D(input_shape = (289,465,3)))
+model.add(tf.keras.layers.Conv2D(16, 3, activation = 'relu'))
 model.add(tf.keras.layers.MaxPooling2D())
-model.add(tf.keras.layers.Conv2D(16, 3, activation = 'relu', input_shape = (645,1168,3)))
+model.add(tf.keras.layers.Conv2D(16, 3, activation = 'relu'))
 model.add(tf.keras.layers.MaxPooling2D())
-model.add(tf.keras.layers.Conv2D(16, 3, activation = 'relu', input_shape = (645,1168,3)))
+model.add(tf.keras.layers.Conv2D(16, 3, activation = 'relu'))
+model.add(tf.keras.layers.MaxPooling2D())
 
 model.add(tf.keras.layers.Flatten())
 
@@ -26,21 +32,21 @@ model.summary()
 
 model.compile(optimizer = 'adam', loss = tf.keras.losses.SparseCategoricalCrossentropy(), metrics = ['accuracy'])
 
-history = model.fit(train_X, train_y, epochs = 1, batch_size = 128, validation_data = (val_X, val_Y))
+history = model.fit(x = train_X, y = train_y, epochs = 1, batch_size = 128, validation_data = (val_X, val_Y))
 
-plt.plot(history.history['accuracy'], label='accuracy')
+"""plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label='val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0, 1])
 plt.legend(loc='lower right')
-plt.show()
+plt.show()"""
 
 predic = model(test_X)
 
 print(np.array(predic[0]))
 
-"""L = [0]*len(predic)
+L = [0]*len(predic)
 
 for i in range (len(predic)):
   L[i] = list(np.array(predic[i])).index(max(np.array(predic[i])))
@@ -49,4 +55,4 @@ cm = confusion_matrix(test_y, L)
 
 class_names = ['Pas feu', 'Feu']
 
-show_confusion_matrix(cm, class_names)"""
+show_confusion_matrix(cm, class_names)
